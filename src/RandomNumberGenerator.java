927bb65032da0d09bbbaeec1f452d3948934c34e
StefanoVuerich <vuric@hotmail.it>
2015-03-27 21:06:19 +0000

    Second commit

--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -1,58 +1,55 @@
 import java.util.Random;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int[] randomNumbers = TheRandomGenerator.generateRandomArray(4);
-		
+
 		String answer1 = "uno";
 		String answer2 = "due";
 		String answer3 = "tre";
 		String answer4 = "quattro";
-		
-		String[] risposte = {answer1, answer2, answer3, answer4};
-		
-		int x = 0;
-		for (int i : randomNumbers) {
-			System.out.println(risposte[i - 1]);
-			++x;
-			
+		String[] answers = { answer1, answer2, answer3, answer4 };
+
+		for (int i : randomNumbers) 
+		{
+			System.out.println(answers[i - 1]);
 		}
 	}
 
-	public static class TheRandomGenerator {
-
-		public static int[] generateRandomArray(int arrayLength) {
-
-			// inizializzo array
+	public static class TheRandomGenerator 
+	{
+		public static int[] generateRandomArray(int arrayLength) 
+		{
 			int[] randomNumbersArray = new int[arrayLength];
 
-			// primo for loop
-			for (int i = 0; i < randomNumbersArray.length; ++i) {
-
+			for (int i = 0; i < randomNumbersArray.length; ++i) 
+			{
 				boolean assigned = false;
 
-				while (!assigned) {
+				while (!assigned) 
+				{
 					Random r = new Random();
 					int randomNumber = r.nextInt((arrayLength + 1) - 1) + 1;
 
 					boolean isNotAlreadyPresent = true;
 
-					for (int j = 0; j <= i; ++j) {
+					for (int j = 0; j <= i; ++j) 
+					{
 						if (randomNumber == randomNumbersArray[j])
 							isNotAlreadyPresent = false;
 					}
-					if (isNotAlreadyPresent) {
+					if (isNotAlreadyPresent) 
+					{
 						randomNumbersArray[i] = randomNumber;
 						assigned = true;
 						break;
 					}
 				}
 			}
-
 			return randomNumbersArray;
 		}
-
 	}
-
 }
